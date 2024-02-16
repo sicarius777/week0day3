88277a857cd9d9d722c2e64f919d099b548f415b
cameronmcnz <you@example.com>
2024-02-15 20:02:44 -0500

    first commit

---
+++ inclass.py
@@ -0,0 +1,37 @@
+obj = [1,2,3]
+
+print(obj)
+
+len (obj)
+
+#function
+#syntax
+#def
+
+def talk():
+    print("........")
+    print("........")
+    print("........")
+    print("........")
+print("Hello")
+talk()
+
+def blender(fruit1, fruit2):
+        print('...blending....')
+        smoothie = 'You made a ' + fruit1 + ' and ' + fruit2 + ' smoothie!'
+        return smoothie
+
+
+s1 = blender("strawberry", "banana")
+s2 = blender("kiwi", "banana")
+s3 = blender("mango", "banana")
+s4 = blender("coconut", "banana")
+s5 = blender("blueberry", "banana")
+
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)
+
+print(blender('passionfruit', 'watermelon'))
